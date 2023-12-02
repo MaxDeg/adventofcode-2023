cc5dba644786f230397cbd81cc2806a1d4e6b61b
MaxDeg <max.degallaix@outlook.com>
2023-12-02 14:31:35 +0100

    implement day 02 part1 & part2 in fsharp

--- fsharp/day-02/day-02/Part1.fs
+++ fsharp/day-02/day-02/Part1.fs
@@ -2,5 +2,65 @@
 
 open System
 open System.Threading.Tasks
+open System
 
-let exec (input: string) (log: string -> unit) = task { return "" }
+open FParsec
+open System
+
+// only 12 red cubes, 13 green cubes, and 14 blue cubes
+
+type CubeSet =
+    { nbOfBlue: int
+      nbOfGreen: int
+      nbOfRed: int }
+
+module CubeSet =
+    let isPossible (cubeSet: CubeSet) =
+        cubeSet.nbOfBlue <= 14 && cubeSet.nbOfGreen <= 13 && cubeSet.nbOfRed <= 12
+
+type GameRecord = int * CubeSet list
+
+module GameRecord =
+    let isPossible (gameRecord: GameRecord) =
+        snd gameRecord |> List.tryFind (not << CubeSet.isPossible) |> Option.isNone
+
+
+let parseInput (input: string) =
+    let parseRecord input : GameRecord =
+        let pcolor color updateState =
+            attempt (pint32 .>> spaces .>> skipStringCI color |>> fun i -> updateState i)
+
+        let pcubeset =
+            let pset =
+                choice
+                    [ pcolor "blue" (fun n -> fun cs -> { cs with nbOfBlue = n })
+                      pcolor "green" (fun n -> fun cs -> { cs with nbOfGreen = n })
+                      pcolor "red" (fun n -> fun cs -> { cs with nbOfRed = n }) ]
+
+            sepEndBy1 (spaces >>. pset .>> spaces) (pstring ",")
+            |>> List.fold
+                    (fun state fn -> fn state)
+                    { nbOfBlue = 0
+                      nbOfGreen = 0
+                      nbOfRed = 0 }
+
+        let parser =
+            skipStringCI "Game" >>. spaces >>. pint32 .>> skipString ":" .>> spaces
+            .>>. sepEndBy1 pcubeset (pstring ";")
+
+        match run parser input with
+        | Success(value, _, _) -> value
+        | Failure(err, _, _) -> failwith err
+
+    input
+    |> _.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries)
+    |> Array.map parseRecord
+
+let exec (input: string) (log: string -> unit) =
+    parseInput input
+    |> Array.filter (fun record ->
+        log (sprintf "%A -> %b" record (GameRecord.isPossible record))
+        GameRecord.isPossible record)
+    |> Array.sumBy fst
+    |> string
+    |> Task.FromResult
--- fsharp/day-02/day-02/Part2.fs
+++ fsharp/day-02/day-02/Part2.fs
@@ -3,4 +3,28 @@ module Part2
 open System
 open System.Threading.Tasks
 
-let exec (input: string) (log: string -> unit) = task { return "" }
+open Part1
+
+module GameRecord =
+    let isPossible (gameRecord: GameRecord) =
+        snd gameRecord |> List.tryFind (not << CubeSet.isPossible)
+
+let exec (input: string) (log: string -> unit) =
+    parseInput input
+    |> Array.map (fun record ->
+        snd record
+        |> List.fold
+            (fun state lowestCubeSet ->
+                { nbOfBlue = Math.Max(state.nbOfBlue, lowestCubeSet.nbOfBlue)
+                  nbOfGreen = Math.Max(state.nbOfGreen, lowestCubeSet.nbOfGreen)
+                  nbOfRed = Math.Max(state.nbOfRed, lowestCubeSet.nbOfRed) })
+            { nbOfBlue = 0
+              nbOfGreen = 0
+              nbOfRed = 0 }
+        |> fun cSet ->
+            let res = cSet.nbOfBlue * cSet.nbOfGreen * cSet.nbOfRed
+            log (sprintf "%A -> %i" cSet res)
+            res)
+    |> Array.sum
+    |> string
+    |> Task.FromResult
